6d7137d923816d091038835155cf72fa0babbc6c
pc <ammar.futur@gmail.com>
2023-12-27 23:08:18 +0100

    Implementing all HTTP requests of Doctor model Testing all HTTP resuequest in browser using swagger API

--- MyAPI/Controllers/DoctorController.cs
+++ MyAPI/Controllers/DoctorController.cs
@@ -3,6 +3,7 @@ using Doctors.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyAPI.Models;
+using MyAPI.Services.Doctor_Request;
 using System.Net;
 using System.Reflection;
 using System.Xml.Linq;
@@ -27,6 +28,18 @@ namespace Doctors.Controllers
             //usinf LINQ to get all the data: using ok response
             return Ok(await _dbContext.Doctors.ToListAsync());
         }
+        //Get Only one doctor
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetDoctor([FromRoute] Guid id)
+        {
+            var doctor = await _dbContext.Doctors.FindAsync(id);
+            if(doctor == null)
+            {
+                return NotFound();
+            }
+            return Ok(doctor);
+        }
         //Add Doctors
         [HttpPost]
         public async Task<IActionResult> AddDoctor(AddDoctorRequest addDoctorRequest)
@@ -46,6 +59,43 @@ namespace Doctors.Controllers
             await _dbContext.SaveChangesAsync();
 
             return Ok(doctor);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateDoctor([FromRoute] Guid id, UpdatDoctorRequest updatDoctorRequest)
+        {
+           var doctor =  _dbContext.Doctors.Find(id);
+            if(doctor != null)
+            {
+                doctor.Name = updatDoctorRequest.Name;
+                doctor.Specialist = updatDoctorRequest.Specialist;
+                doctor.Mobile = updatDoctorRequest.Mobile;
+                doctor.Email = updatDoctorRequest.Email;
+                doctor.Username = updatDoctorRequest.Username;
+                doctor.Password = updatDoctorRequest.Password;
+                doctor.Address = updatDoctorRequest.Address;
+
+                await _dbContext.SaveChangesAsync();
+                return Ok(doctor);
+
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task <IActionResult> DeleteDoctor([FromRoute] Guid id )
+        {
+            var doctor = await _dbContext.Doctors.FindAsync(id);
+            if(doctor != null)
+            {
+                _dbContext.Doctors.Remove(doctor);
+               await _dbContext.SaveChangesAsync();
+
+                return Ok(doctor);
+            }
+            return NotFound();
         }
     }
 }
